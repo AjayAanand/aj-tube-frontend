238a97fdf774b63d1c489be3ed0814b24da87d49
AjStar7 <ajay.anand@ecarasolutions.in>
2025-09-15 18:18:31 +0530

    change name Profile

--- src/screen/main/Profile/profile.tsx
+++ src/screen/main/Profile/profile.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../../../Layout/Layout";
-import "./Profile.css";
+import "./profile.css";
 
 const Profile = () => {
   const api_url = import.meta.env.VITE_API_KEY;
